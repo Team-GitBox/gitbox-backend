5efb07815ebf4442fff4d845841ee346ecad7841
kimday0326 <ktnqkr01@gmail.com>
2024-04-28 19:54:27 +0900

    inital commit

---
+++ src/main/java/com/khu/gitbox/GitboxApplication.java
@@ -0,0 +1,13 @@
+package com.khu.gitbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/khu/gitbox/GitboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khu.gitbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
